3e1b90b3a6051dd79640675e9e0dce1eca2a2435
JKGravelle <gravellejk@g.cofc.edu>
2019-04-15 22:05:45 -0400

    final push

--- app/src/main/java/com/example/lab_03/MainActivity.java
+++ app/src/main/java/com/example/lab_03/MainActivity.java
@@ -1,46 +1,33 @@
 package com.example.lab_03;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
 import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.util.List;
-
-
 public class MainActivity extends AppCompatActivity {
 
-    private String[] classes = {"Japanese 290", "UI Design",
-        "CITA Capstone", "Painting I", "Data Structures and Algorithms",
-            "Art History of Japan", "Printmaking II"};
 
-    public ArrayAdapter<String> arrAdapter;
-    public ListView myList;
-
+    public ArrayAdapter<String> arrayAdapt;
+    public ListView classList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String[] schedule = getResources().getStringArray(R.array.classes);
+        String[] mySchedule = getResources().getStringArray(R.array.myClasses);
 
 
-        arrAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, schedule);
+        arrayAdapt = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, mySchedule);
 
-        myList = findViewById(R.id.class_schedule);
-        myList.setAdapter(arrAdapter);
+        classList = findViewById(R.id.mySchedule);
+        classList.setAdapter(arrayAdapt);
 
-        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        classList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast toast = Toast.makeText(getApplicationContext(), "OnItemClick", Toast.LENGTH_SHORT);
@@ -48,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        myList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        classList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast toast = Toast.makeText(getApplicationContext(), "OnItemLongClick", Toast.LENGTH_SHORT);
